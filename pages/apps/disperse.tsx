0622dbb62e7067b118d3fa97d48bd6492ad0655c
Daniil Polienko <donyaschefer@gmail.com>
2024-04-04 22:23:29 +0300

    chore: remove old getLayout

--- pages/apps/disperse.tsx
+++ pages/apps/disperse.tsx
@@ -1,5 +1,4 @@
 import React, {Fragment} from 'react';
-import {DefaultSeo} from 'next-seo';
 import Disperse from 'components/sections/Disperse/index';
 import {DisperseContextApp} from 'components/sections/Disperse/useDisperse';
 import {DisperseQueryManagement} from 'components/sections/Disperse/useDisperseQuery';
@@ -23,40 +22,6 @@ function DispersePage(): ReactElement {
 		</DisperseContextApp>
 	);
 }
-
-DispersePage.getLayout = function getLayout(page: ReactElement): ReactElement {
-	return (
-		<Fragment>
-			<DefaultSeo
-				title={'Disperse - SmolDapp'}
-				defaultTitle={'Disperse - SmolDapp'}
-				description={'Distribute ether or tokens to multiple addresses'}
-				openGraph={{
-					type: 'website',
-					locale: 'en-US',
-					url: 'https://disperse.smold.app',
-					site_name: 'Disperse - SmolDapp',
-					title: 'Disperse - SmolDapp',
-					description: 'Distribute ether or tokens to multiple addresses',
-					images: [
-						{
-							url: 'https://smold.app/og_disperse.png',
-							width: 800,
-							height: 400,
-							alt: 'disperse'
-						}
-					]
-				}}
-				twitter={{
-					handle: '@smoldapp',
-					site: '@smoldapp',
-					cardType: 'summary_large_image'
-				}}
-			/>
-			{page}
-		</Fragment>
-	);
-};
 
 DispersePage.AppName = 'Disperse';
 DispersePage.AppDescription = 'Transfer funds to multiple receivers';
