1acb227edad3c8e678ce196307a1aff94a5a94ff
Majorfi <major@ycorpo.com>
2024-05-03 10:13:40 +0200

    chore: test deploy config

--- packages/smol/tailwind.config.js
+++ packages/smol/tailwind.config.js
@@ -1,1 +1,15 @@
-module.exports = require('../lib/tailwind.config');
+const defaultConfig = require('../lib/tailwind.config');
+
+module.exports = {
+	content: [
+		...defaultConfig.content,
+		'./components/**/*.{js,ts,jsx,tsx}',
+		'./contexts/**/*.{js,ts,jsx,tsx}',
+		'./hooks/**/*.{js,ts,jsx,tsx}',
+		'./pages/**/*.{js,ts,jsx,tsx}'
+	],
+	theme: {
+		...defaultConfig.theme
+	},
+	plugins: [...defaultConfig.plugins]
+};
