4225fe691c4bf0c4d3d163912b082d298747e459
Majorfi <major@ycorpo.com>
2024-05-03 12:40:48 +0200

    chore: test deploy config

--- packages/lib/icons/IconSpinner.tsx
+++ packages/lib/icons/IconSpinner.tsx
@@ -6,7 +6,7 @@ function IconSpinner(props: React.SVGProps<SVGSVGElement>): ReactElement {
 	return (
 		<svg
 			{...props}
-			className={'h-4 w-4 animate-spin text-neutral-900'}
+			className={'size-4 animate-spin text-neutral-900'}
 			xmlns={'http://www.w3.org/2000/svg'}
 			fill={'none'}
 			viewBox={'0 0 24 24'}>
@@ -16,15 +16,15 @@ function IconSpinner(props: React.SVGProps<SVGSVGElement>): ReactElement {
 				cy={'12'}
 				r={'10'}
 				stroke={'currentColor'}
-				strokeWidth={'4'}>
-   </circle>
+				strokeWidth={'4'}
+			/>
 			<path
 				className={'opacity-75'}
 				fill={'currentColor'}
 				d={
 					'M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-				}>
-   </path>
+				}
+			/>
 		</svg>
 	);
 }
