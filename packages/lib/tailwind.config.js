154fce7841cd7829a3e781498291c9e291b6bc59
Majorfi <major@ycorpo.com>
2024-05-07 16:07:34 +0200

    feat: wip general config

--- packages/lib/tailwind.config.js
+++ packages/lib/tailwind.config.js
@@ -4,13 +4,11 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
 	content: [
-		'../smol/components/**/*.{js,ts,jsx,tsx}',
-		'../smol/contexts/**/*.{js,ts,jsx,tsx}',
-		'../smol/hooks/**/*.{js,ts,jsx,tsx}',
-		'../smol/pages/**/*.{js,ts,jsx,tsx}',
-		'../gimme/pages/**/*.{js,ts,jsx,tsx}',
-		'../gimme/hooks/**/*.{js,ts,jsx,tsx}',
-		'../lib/**/*.{js,ts,jsx,tsx}'
+		'../lib/icons/**/*.{js,jsx,ts,tsx}',
+		'../lib/common/**/*.{js,jsx,ts,tsx}',
+		'../lib/primitives/**/*.{js,jsx,ts,tsx}',
+		'../lib/types/**/*.{js,jsx,ts,tsx}',
+		'../lib/utils/**/*.{js,jsx,ts,tsx}'
 	],
 	theme: {
 		colors: {
@@ -22,12 +20,11 @@ module.exports = {
 			primaryHover: '#FFE043',
 			neutral: {
 				0: '#FFFFFF',
-				// 50: '', unavailable
-				// 100: '', unavailable
+				100: '#F9F9F9',
 				200: '#F7F7F7',
 				300: '#F3F3F3',
 				400: '#DCDDDD',
-				// 500: '', unavailable
+				500: '#ADB1BD',
 				600: '#ADB1BD',
 				700: '#474F59',
 				800: '#272B30',
--- packages/multisafe/tailwind.config.js
+++ packages/multisafe/tailwind.config.js
@@ -1,95 +1,14 @@
-const plugin = require('tailwindcss/plugin');
-const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
+/** @type {import('tailwindcss').Config} */
+const config = require('../lib/tailwind.config');
 
 module.exports = {
+	...config,
 	content: [
 		'./components/**/*.{js,ts,jsx,tsx}',
-		'./components/**/**/*.{js,ts,jsx,tsx}',
 		'./contexts/**/*.{js,ts,jsx,tsx}',
 		'./hooks/**/*.{js,ts,jsx,tsx}',
-		'./pages/**/*.{js,ts,jsx,tsx}'
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		...config.content
 	],
-	theme: {
-		colors: {
-			black: 'hsl(0, 0%, 0%)',
-			white: 'rgb(255, 255, 255)',
-			transparent: 'transparent',
-			inherit: 'inherit',
-			primary: '#FFD915',
-			primaryHover: '#FFE043',
-			orange: {
-				50: '#fff7ed',
-				100: '#ffedd5',
-				200: '#fed7aa',
-				300: '#fdba74',
-				400: '#fb923c',
-				500: '#f97316',
-				600: '#ea580c',
-				700: '#c2410c',
-				800: '#9a3412',
-				900: '#7c2d12',
-				950: '#431407'
-			},
-			neutral: {
-				0: '#FFFFFF',
-				// 50: '', unavailable
-				100: '#FAFAFA',
-				200: '#F7F7F7',
-				300: '#F3F3F3',
-				400: '#DCDDDD',
-				500: '#ADB1BD',
-				600: '#ADB1BD',
-				700: '#474F59',
-				800: '#272B30',
-				900: '#060B11'
-			},
-			pink: colors.pink,
-			red: colors.rose,
-			green: colors.emerald,
-			blue: colors.blue,
-			teal: colors.teal
-		},
-		extend: {
-			fontFamily: {
-				sans: ['var(--inter-font)', 'Inter', 'Roboto', ...defaultTheme.fontFamily.sans],
-				mono: ['Source Code Pro', ...defaultTheme.fontFamily.mono]
-			},
-			width: {
-				inherit: 'inherit'
-			},
-			fontSize: {
-				xxs: ['10px', '16px'],
-				xs: ['12px', '16px'],
-				sm: ['14px', '20px'],
-				base: ['16px', '24px'],
-				intermediate: ['18px', '24px'],
-				lg: ['20px', '32px'],
-				xl: ['24px', '32px'],
-				'3xl': ['32px', '40px'],
-				'4xl': ['40px', '56px'],
-				'7xl': ['80px', '96px']
-			},
-			maxWidth: {
-				xl: '552px',
-				'4xl': '904px',
-				'6xl': '1200px'
-			}
-		}
-	},
-	plugins: [
-		require('@tailwindcss/forms'),
-		require('@tailwindcss/typography'),
-		plugin(function ({addUtilities}) {
-			addUtilities({
-				'.scrollbar-none': {
-					'-ms-overflow-style': 'none',
-					'scrollbar-width': 'none',
-					'&::-webkit-scrollbar': {
-						display: 'none'
-					}
-				}
-			});
-		})
-	]
+	plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('tailwindcss-animate')]
 };
