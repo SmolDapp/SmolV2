0c9dfb60a1c24fd9dbf4eb10d6a69acaba83e0f8
Majorfi <major@ycorpo.com>
2024-05-15 22:34:33 +0200

    feat: twitter setup

--- packages/lib/utils/twitter.ts
+++ packages/lib/utils/twitter.ts
@@ -1,5 +1,6 @@
 export const TWEETER_SHARE_CONTENT = {
 	SWAP: 'I%20just%20swapped%20on%20Smold.app%2C%20the%20homepage%20for%20your%20crypto%20adventure.%20Maybe%20you%E2%80%99ll%20see%20this%20tweet%20and%20use%20Smol%20too%3F%20Wouldn%E2%80%99t%20that%20be%20nice%21',
-	SEND: '',
-	DISPERSE: ''
+	SEND: 'I%20just%20sent%20tokens%20on%20Smold.app%2C%20the%20easiest%20way%20to%20send%20crypto%20around.%20Facts%21%20If%20you%20like%20sending%20stuff%2C%20maybe%20you%20should%20check%20it%20out%21',
+	DISPERSE:
+		'I%20just%20used%20disperse%20on%20Smold.app%2C%20the%20easiest%20way%20to%20send%20tokens%20to%20muliple%20addresses%20in%20a%20single%20transaction.%20Fancy%21%20If%20you%20read%20this%20tweet%20you%20definitely%20shouldn%27t%20check%20out%20Smold.app.%20%28Reverse%20psychology%20innit%29.'
 };
