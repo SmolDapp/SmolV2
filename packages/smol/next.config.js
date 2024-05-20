a0dd80b106b359c8b4fa91cffa53c71ae778e137
Majorfi <major@ycorpo.com>
2024-05-20 10:52:47 +0200

    feat: RPC

--- packages/smol/next.config.js
+++ packages/smol/next.config.js
@@ -101,6 +101,42 @@ module.exports = phase =>
 			},
 			env: {
 				PROJECT_SLUG: 'smoldapp',
+				/**********************************************************************************
+				 ** New RPC Setup for mainnet networks
+				 *********************************************************************************/
+				RPC_URI_FOR_1: process.env.RPC_URI_FOR_1,
+				RPC_URI_FOR_10: process.env.RPC_URI_FOR_10,
+				RPC_URI_FOR_56: process.env.RPC_URL_BINANCE,
+				RPC_URI_FOR_137: process.env.RPC_URI_FOR_137,
+				RPC_URI_FOR_250: process.env.RPC_URI_FOR_250,
+				RPC_URI_FOR_252: process.env.RPC_URI_FOR_252,
+				RPC_URI_FOR_288: process.env.RPC_URI_FOR_288,
+				RPC_URI_FOR_8453: process.env.RPC_URI_FOR_8453,
+				RPC_URI_FOR_42161: process.env.RPC_URI_FOR_42161,
+				RPC_URI_FOR_42170: process.env.RPC_URI_FOR_42170,
+				RPC_URI_FOR_56288: process.env.RPC_URI_FOR_56288,
+				RPC_URI_FOR_81457: process.env.RPC_URI_FOR_81457,
+				RPC_URI_FOR_111188: process.env.RPC_URI_FOR_111188,
+
+				/**********************************************************************************
+				 ** New RPC Setup for testnet networks
+				 *********************************************************************************/
+				RPC_URI_FOR_97: process.env.RPC_URL_BINANCE_TESTNET,
+				RPC_URI_FOR_400: process.env.RPC_URL_OPTIMISM_GOERLI,
+				RPC_URI_FOR_2522: process.env.RPC_URI_FOR_2522,
+				RPC_URI_FOR_9728: process.env.RPC_URI_FOR_9728,
+				RPC_URI_FOR_17000: process.env.RPC_URI_FOR_17000,
+				RPC_URI_FOR_18233: process.env.RPC_URI_FOR_18233,
+				RPC_URI_FOR_28882: process.env.RPC_URI_FOR_28882,
+				RPC_URI_FOR_80001: process.env.RPC_URI_FOR_80001,
+				RPC_URI_FOR_84532: process.env.RPC_URI_FOR_84532,
+				RPC_URI_FOR_421614: process.env.RPC_URI_FOR_421614,
+				RPC_URI_FOR_11155111: process.env.RPC_URI_FOR_11155111,
+				RPC_URI_FOR_11155420: process.env.RPC_URI_FOR_11155420,
+
+				/**********************************************************************************
+				 ** Legacy RPC configuration, mainnet and testnet
+				 *********************************************************************************/
 				JSON_RPC_URL: {
 					1: process.env.RPC_URL_MAINNET,
 					10: process.env.RPC_URL_OPTIMISM,
@@ -114,7 +150,9 @@ module.exports = phase =>
 					42161: process.env.RPC_URL_ARBITRUM,
 					11155111: process.env.RPC_URL_SEPOLIA
 				},
-				// Wallet Connect modal configuration
+				/**********************************************************************************
+				 ** Wallet Connect configuration
+				 *********************************************************************************/
 				WALLETCONNECT_PROJECT_ID: process.env.WALLETCONNECT_PROJECT_ID,
 				WALLETCONNECT_PROJECT_NAME: 'Smol',
 				WALLETCONNECT_PROJECT_DESCRIPTION:
