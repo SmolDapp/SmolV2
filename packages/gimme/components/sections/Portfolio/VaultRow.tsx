9194de805f42f023c79bae61edf3629ef2bad75d
Daniil Polienko <donyaschefer@gmail.com>
2024-05-22 17:46:44 +0300

    feat: add network switch on vault actions

--- packages/gimme/components/sections/Portfolio/VaultRow.tsx
+++ packages/gimme/components/sections/Portfolio/VaultRow.tsx
@@ -1,16 +1,51 @@
 import {type ReactElement} from 'react';
-import Link from 'next/link';
-import {formatCounterValue, formatTAmount, percentOf} from '@builtbymom/web3/utils';
+import toast from 'react-hot-toast';
+import {useRouter} from 'next/router';
+import {useAccount, useSwitchChain} from 'wagmi';
+import {useWeb3} from '@builtbymom/web3/contexts/useWeb3';
+import {formatCounterValue, formatTAmount, percentOf, toAddress} from '@builtbymom/web3/utils';
 import {Counter} from '@gimmeDesignSystem/Counter';
 import {ImageWithFallback} from '@lib/common/ImageWithFallback';
 import {supportedNetworks} from '@lib/utils/tools.chains';
 
-import type {TNormalizedBN} from '@builtbymom/web3/types';
+import type {BaseError} from 'wagmi';
+import type {TAddress, TNormalizedBN} from '@builtbymom/web3/types';
 import type {TYDaemonVault} from '@yearn-finance/web-lib/utils/schemas/yDaemonVaultsSchemas';
 
 export function VaultRow(props: {vault: TYDaemonVault; balance: TNormalizedBN; price?: TNormalizedBN}): ReactElement {
 	const vaultChainName = supportedNetworks.find(network => network.id === props.vault.chainID)?.name;
 	const tokenNetworkString = `${props.vault.token.symbol} on ${vaultChainName}`.toLocaleUpperCase();
+	const router = useRouter();
+	const {chainID} = useWeb3();
+
+	const {connector} = useAccount();
+
+	const {switchChainAsync} = useSwitchChain();
+
+	const onAction = async ({
+		tokenAddress,
+		vaultAddress
+	}: {
+		tokenAddress: TAddress;
+		vaultAddress?: TAddress;
+	}): Promise<void> => {
+		try {
+			const URLQueryParam = new URLSearchParams();
+			URLQueryParam.set('tokenAddress', toAddress(tokenAddress));
+			vaultAddress && URLQueryParam.set('vaultAddress', toAddress(vaultAddress));
+
+			if (props.vault.chainID !== chainID) {
+				await switchChainAsync({connector, chainId: props.vault.chainID});
+			}
+
+			router.push({
+				pathname: '/earn',
+				query: URLQueryParam.toString()
+			});
+		} catch (err) {
+			toast.error((err as BaseError)?.message || 'An error occured while creating your transaction!');
+		}
+	};
 
 	return (
 		<div className={'grid w-full grid-cols-12 justify-between rounded-md border border-neutral-400 p-6'}>
@@ -57,22 +92,22 @@ export function VaultRow(props: {vault: TYDaemonVault; balance: TNormalizedBN; p
 					</p>
 				</div>
 				<div className={'group col-span-2 flex flex-row items-center justify-end gap-2'}>
-					<Link href={`/earn?tokenAddress=${props.vault.address}`}>
-						<button
-							className={
-								'relative flex size-6 items-center justify-center rounded-full border border-neutral-600 transition-colors hover:bg-neutral-200'
-							}>
-							<p className={'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'}>{'-'}</p>
-						</button>
-					</Link>
-					<Link href={`/earn?tokenAddress=${props.vault.token.address}&vaultAddress=${props.vault.address}`}>
-						<button
-							className={
-								'relative mr-2 flex size-6 items-center justify-center rounded-full border border-neutral-600 transition-colors hover:bg-neutral-200'
-							}>
-							<p className={'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'}>{'+'}</p>
-						</button>
-					</Link>
+					<button
+						onClick={async () => onAction({tokenAddress: props.vault.address})}
+						className={
+							'relative flex size-6 items-center justify-center rounded-full border border-neutral-600 transition-colors hover:bg-neutral-200'
+						}>
+						<p className={'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'}>{'-'}</p>
+					</button>
+					<button
+						onClick={async () =>
+							onAction({tokenAddress: props.vault.token.address, vaultAddress: props.vault.address})
+						}
+						className={
+							'relative mr-2 flex size-6 items-center justify-center rounded-full border border-neutral-600 transition-colors hover:bg-neutral-200'
+						}>
+						<p className={'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'}>{'+'}</p>
+					</button>
 				</div>
 			</div>
 		</div>
