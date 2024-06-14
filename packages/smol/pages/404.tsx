a0d43c1b955b496c61c3fd05c269f28b010b5686
Karlen9 <misakyankarlen@gmail.com>
2024-06-14 15:51:37 +0300

    feat: 404

---
+++ packages/smol/pages/404.tsx
@@ -0,0 +1,31 @@
+import {type ReactElement} from 'react';
+import Link from 'next/link';
+import {IconSmolMouse} from '@lib/icons/IconSmolMouse';
+import {Button} from '@lib/primitives/Button';
+
+export default function NotFoundPage(): ReactElement {
+	return (
+		<div
+			className={
+				'flex h-[calc(100vh-500px)] w-full flex-col items-center  justify-center md:h-[calc(100vh-350px)]'
+			}>
+			<div className={'relative'}>
+				<p
+					className={
+						'text-[100px] font-bold leading-[100px] text-neutral-800 md:text-[200px] md:leading-[200px]'
+					}>
+					{'404'}
+				</p>
+				<IconSmolMouse
+					className={'absolute left-1/2 top-2/3 size-10 -translate-x-1/2 -translate-y-2/3 md:size-24'}
+				/>
+			</div>
+			<Link href={'/apps/wallet'}>
+				<Button className={'!h-8'}>{'Go Back'}</Button>
+			</Link>
+		</div>
+	);
+}
+
+NotFoundPage.AppName = 'Nothing here!';
+NotFoundPage.AppDescription = 'We can’t find the page you’re looking for. But here’s a smol mouse!';
